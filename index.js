d8e28de3c43ef683fbf4c5795d5d47e79ddc8a5e
Giannis Mousouleas <yannismousouleas@gmail.com>
2024-09-05 11:30:17 +0000

    Initial commit

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
 
+const multer  = require('multer')
+const upload = multer({ dest: './public/data/uploads/' })
+
+
 var app = express();
 
 app.use(cors());
@@ -12,6 +16,14 @@ app.get('/', function (req, res) {
 });
 
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res)=>{
+  res.json({
+    name: req.file.originalname, 
+    type: req.file.mimetype,  
+    size: req.file.size
+  });
+});
+  
 
 
 const port = process.env.PORT || 3000;
